590aafa1a11e78ffcbe8dee581561db81dde27a3
108M <117757556+108M@users.noreply.github.com>
2022-11-08 17:11:17 +0100

    Add files via upload

---
+++ colaEnteros.h
@@ -0,0 +1,45 @@
+/*
+  FICHERO: colaEnteros.h
+  VERSION: 1.0.0
+  HISTORICO:
+  Creado por Aránzazu Jurío Munárriz el 09/09/19.
+  
+  Este fichero se crea sólo con intenciones de coordinación docente y como
+  ayuda a sus alumnos y alumnas. La autora desautoriza expresamente su difusión, copia
+  o exhibición pública (salvo entre los alumnos de las asignaturas 240301 y 250301 del
+  grado en Ingeniería Informática de la UPNA).
+*/
+
+#include <stdbool.h>
+
+ typedef int tipoElementoCola;
+ typedef struct Jugador{
+ 	 tipoElementoCola FP;
+     tipoElementoCola TP;
+     tipoElementoCola Patada;
+	 tipoElementoCola Cuerpo;
+	 tipoElementoCola Control;
+	 tipoElementoCola Guardia;
+	 tipoElementoCola Velocidad;
+	 tipoElementoCola Estamina;
+	 tipoElementoCola Valor;
+ }jugador;
+ 
+typedef struct celdaC{
+ 	jugador* jg; 
+ 	struct celdaC *sig;
+ } celdaCola; 
+typedef struct tipoC{
+	celdaCola* ini;
+	celdaCola* fin;
+}tipoCola;
+
+void nuevaCola(tipoCola *);
+
+void encolar(tipoCola *, int jugador [8]);
+
+void desencolar(tipoCola *);
+
+tipoElementoCola frente(tipoCola);
+
+bool esNulaCola(tipoCola);
